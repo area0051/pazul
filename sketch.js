f34da890f2ccd1c91693eaae4088bcfd55e0cde6
area0051 <109328485+area0051@users.noreply.github.com>
2023-11-24 02:06:29 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,98 +1,104 @@
-let slotList = [
-  -1, -1, -1, -1, -1, -1, -1, -1, -1,
-  -1, -1, -1,  1,  1,  1, -1, -1, -1,
-  -1, -1, -1,  1,  1,  1, -1, -1, -1,
-  -1,  1,  1,  1,  1,  1,  1,  1, -1,
-  -1,  1,  1,  1,  0,  1,  1,  1, -1,
-  -1,  1,  1,  1,  1,  1,  1,  1, -1,
-  -1, -1, -1,  1,  1,  1, -1, -1, -1,
-  -1, -1, -1,  1,  1,  1, -1, -1, -1,
-  -1, -1, -1, -1, -1, -1, -1, -1, -1,
-];
-
-class History{
-  constructor(){
-    this.slotList = [];
-  }
+// This app is a Audio Spectrum with p5.sound.
+// The color will change with a amp volume.
+class Lyric{
+	constructor(body, t){
+		this.body = body;
+		this.t = t;
+	}
 }
-let historyList = [];
-let bango = -99;
 
+
+let sound;
+let fft;
+let amplitude;
+let lyricList = [];
+function preload() {
+  sound = loadSound('https://tentouser.github.io/jsonapi/sounds/potato.mp3');
+}
 function setup() {
-    createCanvas(600, 400);
+  createCanvas(600, 400);
+  
+	fft = new p5.FFT();
+	amplitude = new p5.Amplitude();
+	
+	lyricList = createLyricList();
 }
-
 function draw() {
-    background(255, 200, 0);
-    for (let i = 0; i < 81; i++) {
-      if(slotList[i] == -1){
-        continue;
-      }
-      let x = 140 + i % 9 * 40;
-      let y = 40 + int(i / 9) * 40;
-      fill(255, 255, 255);
-      ellipse(x, y, 35, 35);
-      
-      if(slotList[i] == 1){
-        fill(0,200,255);
-        if(i == bango){
-          fill(0,255,255);
-        }
-        ellipse(x, y, 35, 35);
-      }
+  background(30);
+  let spectrum = fft.analyze();
+	
+  noStroke();
+  fill(255);
+	
+	let from = color(0, 255, 200);
+	let to = color(255, 100, 0);
+	let level = map(amplitude.getLevel(), 0, 0.4, 0, 1);
+	let c = makeColor(from, to, level);
+  for (let i = 0; i < spectrum.length; i++) {
+		if(i % 50 == 0){
+			
+			let x = map(i, 0, spectrum.length, 0, width);
+			let per = spectrum[i] / 255;
+			let h = per * 100;
+			h = max(h, 1);
+			let y = 200;
+			
+			// let c = makeColor(from, to, i / 1024);
+			fill(c);
+			rect(x, y, width / spectrum.length * 30, h, 0, 0, 5, 5);
+			rect(x, y, width / spectrum.length * 30, -h, 0, 0, 5, 5);
+		}
   }
+	
+	textAlign(CENTER, CENTER);
+	textSize(16);
+	fill(120);
+	text(getLyric(lyricList, sound.currentTime()), width/2, height - 50);
+}
+function mousePressed(){
+	if(sound.isPlaying() == false){
+		sound.play();
+	}
 }
 
-function mousePressed() {
-  for (let i = 0; i < 81; i++) {
-    if (slotList[i] == -1) {
-      continue;
-    }
-    let x = 140 + (i % 9) * 40;
-    let y = 40 + int(i / 9) * 40;
-    let kyori = dist(mouseX, mouseY, x, y);
-    if (kyori < 20) {
-      if (bango == -99) {
-        bango = i;
-      } else {
-        move(bango, i);
-        bango = -99;
-      }
-    }
-  }
-}
-
-function keyPressed(){
-  if(key == 'u'){
-    if(historyList.length > 0){
-      let history = historyList[historyList.length -1];
-      slotList = history.slotList;
-      historyList.pop();
-    }
-  }
+function makeColor(from, to, per){
+	let diff_r = red(to) - red(from);
+	let diff_g = green(to) - green(from);
+	let diff_b = blue(to) - blue(from);
+	let r = red(from) + per * diff_r;
+	let g = green(from) + per * diff_g;
+	let b = blue(from) + per * diff_b;
+	return color(r, g, b);
 }
 
 
-
-function move(from, to) {
-  let diff = to - from;
-  let directions = [-9, 9, 1, -1];
-  for (let i = 0; i < 4; i++) {
-    let dir = directions[i];  // Corrected index here
-    if(diff == dir * 2){
-      if (slotList[from + dir] == 1) {
-        if (slotList[from + dir * 2] == 0) {
-          //現在の配列をきろくさせていく
-          let history = new History();
-          history.slotList = slotList.slice();
-          historyList.push(history)
-          
-          
-          slotList[from] = 0;
-          slotList[from + dir] = 0;  // Corrected index here
-          slotList[from + dir * 2] = 1;
-        }
-      }
-    }
-  }
+function getLyric(lyricList, t){
+	let lyric = null;
+	for(let i=0; i<lyricList.length; i++){
+		if(lyricList[i].t < t){
+			lyric = lyricList[i];
+		}
+	}
+	if(lyric == null){
+		return "";
+	}
+	return lyric.body;
 }
+
+function createLyricList(){
+	let list = [];
+	list.push(new Lyric("♪ I'm lovin' it - Composed by Hot Dog -", 0));
+	list.push(new Lyric("マックのポテト揚げたて食べたい", 17));
+	list.push(new Lyric("マックのポテト揚げ直せ", 22));
+	list.push(new Lyric("マックのポテト揚げたて食べたい", 26));
+	list.push(new Lyric("揚げたてないなら", 30));
+	list.push(new Lyric("いらねぇ", 32));
+	list.push(new Lyric("バーガーいらねぇ　ポテトを出せ", 34));
+	list.push(new Lyric("バーガーいらねぇ　揚げ直せ", 37));
+	list.push(new Lyric("バーガーいらねぇ　ポテトを出せ", 41));
+	list.push(new Lyric("バーガーいらねぇ　金返せ", 45));
+	list.push(new Lyric("fin.", 50));
+	return list;
+}
+
+
